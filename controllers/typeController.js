1a37b94374fd69b2bcf30c5e7e7d4bc70cd50e61
DashinIN <green19grant@yandex.ru>
2023-08-29 11:16:08 +0300

    fix controller

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -9,7 +9,7 @@ class TypeController {
     }
 
     async getAll(req, res) {
-        const types = (await Type.findAll()).length()
+        const types = await Type.findAll()
         return res.json(types)
     }
 
